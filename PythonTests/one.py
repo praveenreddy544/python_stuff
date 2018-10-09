76b4d606e76dd5b78f6b70c044bec9387e9febc4
chintu <praveenporeddy8@.com>
2018-10-09 17:30:04 -0400

    added secret number test code

---
+++ PythonTests/one.py
@@ -0,0 +1,6 @@
+def printing(str):
+    print "test is",str
+    return 3
+def value(str):
+    print "defined value is",str
+    return 4
---
+++ PythonTests/two.py
@@ -0,0 +1,4 @@
+#import one
+from one import value
+#one.printing("testing stuff")
+one.value("hola")
