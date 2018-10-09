ff52894deec742edf423885ed412ed023dde6885
chintu <praveenporeddy8@.com>
2018-10-09 17:36:26 -0400

    added code to secret_number

--- practice_scripts/secret_number.py
+++ practice_scripts/secret_number.py
@@ -1,3 +1,12 @@
 import random
-a=random.randint(1,10)
-print(a)
+secret_number=random.randint(1,10)
+#print(f"secret number generated is ------> {secret_number}")
+for i in range(3):
+    guess_number=int(input("enter your guess number"))
+    if guess_number == secret_number :
+        print("your guessed number matched with secret number")
+    elif guess_number < secret_number:
+        print("your guessed number is less than secret number")
+    elif guess_number > secret_number:
+        print("your guessed number is more than secret number")
+
